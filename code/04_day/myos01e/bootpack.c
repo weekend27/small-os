4cfd96f08afb1980837fa31db538a87974d1598a
weekend27 <weekend27@163.com>
2015-06-02 20:49:23 +0800

    04_day

---
+++ code/04_day/myos01e/bootpack.c
@@ -0,0 +1,18 @@
+//告诉C编译器，有一个函数在别的文件中
+void io_hlt(void);
+
+void HariMain(void){
+	int i; //变量声明：i是一个32位整数
+	char *p; //变量p，用于BYTE型地址
+	
+	p = (char *)0xa0000;  //给地址变量赋值
+	
+	for(i = 0; i <= 0xffff; i++){
+		//搞搞条纹图案
+		p[i] = i & 0xf5;
+	}
+	
+	for(;;){
+		io_hlt();
+	}
+}
