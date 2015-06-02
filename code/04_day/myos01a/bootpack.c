a0d9a6811bf2c891871d9a0b8175021ec96102de
weekend27 <weekend27@163.com>
2015-06-02 11:04:05 +0800

    04_day

---
+++ code/04_day/myos01a/bootpack.c
@@ -0,0 +1,15 @@
+//告诉C编译器，有一个函数在别的文件中
+void io_hlt(void);
+void write_mem8(int addr, int data);
+
+void HariMain(void){
+	int i; //变量声明：i是一个32位整数
+	
+	for(i = 0xa0000; i <= 0xaffff; i++){
+		write_mem8(i,15);		//15是纯白色， MOV BYTE [i],15
+	}
+	
+	for(;;){
+		io_hlt();
+	}
+}
