4d2271811bd3a6898e01a0e544b4864e40894680
weekend27 <weekend27@163.com>
2015-06-04 20:10:07 +0800

    06_day

---
+++ code/06_day/myos03c/bootpack.c
@@ -0,0 +1,33 @@
+/**
+*bootpack.c 其他处理，像是Controller
+*/
+#include "bootpack.h"
+#include <stdio.h>
+
+void HariMain(void){
+	
+	struct BOOTINFO *binfo = (struct BOOTINFO *)0x0ff0;
+	char s[50], mcursor[256];
+	int mx, my;
+	
+	init_palette();
+	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+	
+	//先将鼠标位置放在窗口中央
+	mx = (binfo->scrnx - 16) / 2;
+	my = (binfo->scrny - 28 - 16) / 2;
+	
+	init_mouse_cursor8(mcursor, COL8_008484);
+	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+	sprintf(s, "(%d, %d)", mx, my);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+	
+	for(;;){
+		io_hlt();
+	}
+}
+
+
+
+
+
