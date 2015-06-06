0b2cb074727e3c001cda6b06fd6a8aba15c0cc71
weekend27 <weekend27@163.com>
2015-06-06 12:08:57 +0800

    07_day

--- myos04b/int.c
+++ myos04b/int.c
@@ -24,6 +24,9 @@ void init_pic(void){
 	return;
 }
 
+struct KEYBUF{
+	unsigned char data, flag;
+};
 
 #define PORT_KEYDAT		0x0060
 
@@ -32,7 +35,7 @@ struct KEYBUF keybuf;
 void inthandler21(int *esp){
 	
 	unsigned char data;
-	io_out8(PIC0_OCW2,0x61);			/* 通知PIC IRQ-01已经受理完毕 */
+	io_out8(PIC_OCW2,0x61);			/* 通知PIC IRQ-01已经受理完毕 */
 	data = io_in8(PORT_KEYDAT);
 	if(keybuf.flag == 0){
 		keybuf.data = data;
